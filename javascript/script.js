113ec6881cfefe6f729d1a6bc6b3c7035a73225c
sreeramk28 <43648958+sreeramk28@users.noreply.github.com>
2023-07-25 06:04:33 +0530

    brand new ui features (#1)

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,51 @@
-document.addEventListener("DOMContentLoaded", showTopPlayers());
+const blitzOption = document.getElementById("blitz");
+blitzOption.addEventListener("change", showTopPlayers);
 
 async function showTopPlayers() {
-  const response = await fetch("https://bccleaderboard.onrender.com/bestPlayers");
+  
+  let tableContainer = document.getElementById("container");
+  
+  /* Show a spinner */
+  let spinner = document.createElement("div");
+  spinner.classList.add("spinner-border");
+  spinner.classList.add("text-light");
+  tableContainer.appendChild(spinner);
+  
+  const PROD_BASE_URL = "https://bccleaderboard.onrender.com";
+  const LOCAL_BASE_URL = "http://localhost:8080";
+  
+  const response = await fetch(PROD_BASE_URL + "/bestPlayers");
   const jsonData = await response.json();
 
+  /* Remove Spinner and show data */
+  tableContainer.removeChild(spinner);
+
+  tableContainer.innerHTML = `
+    <table id="board" class="table table-dark text-center table-transparency">
+    </table>
+  `;
+  
   let playerArray = []
   jsonData.topPlayers.forEach(player => {
     playerArray.push([player.username, player.clubScoreMonthAggregate, player.tournamentPointsMonthAggregate, player.tiebreakPointsMonthAggregate]);
   });
-  let table = document.getElementById("boardbody");
+
+  let table = document.getElementById("board");
+  table.innerHTML = `
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Player</th>
+        <th scope="col">Ranking points</th>
+        <th scope="col">TB1</th>
+        <th scope="col">TB2</th>
+      </tr>
+    </thead>
+    <tbody id="boardbody">
+    </tbody>
+  `;
+  
+  let tableBody = document.getElementById("boardbody");
   for (let i = 0; i < playerArray.length; i++) {
     let tr = document.createElement("tr");
     let th = document.createElement("th");
@@ -20,6 +57,6 @@ async function showTopPlayers() {
       td.innerText = val;
       tr.appendChild(td);
     });
-    table.appendChild(tr);
+    tableBody.appendChild(tr);
   }
 }
